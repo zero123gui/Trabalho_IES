34b0f11ffa6e89b2617f8cc01b6218120c3122a9
guilherme_miller <gktmiller@gmail.com>
2022-11-10 07:47:10 -0300

    ex4 parametros feito e comentado

---
+++ Parametros/parametros_ex4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define MAX 1000
+typedef struct{
+    int nota1,nota2,nota3;
+}NOTAS;
+void scanvet(NOTAS *n){
+    scanf("%d",&n->nota1);
+    scanf("%d",&n->nota2);
+    scanf("%d",&n->nota3);
+}
+void pnotas(NOTAS n){
+    printf("%d\n",n.nota1);
+    printf("%d\n",n.nota2);
+    printf("%d\n",n.nota3);
+}
+int main(){
+    NOTAS n;
+    scanvet(&n);
+    pnotas(n);
+    
+return 0;
+}
