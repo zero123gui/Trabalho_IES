af22af6ad792e560de7edc75ea86ec5cff9626f7
guilherme_miller <gktmiller@gmail.com>
2022-11-10 07:48:16 -0300

    ex8 strings feito e

---
+++ Strings/Str_ex8.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+
+int main() {
+    char texto[MAX];
+    int tamanho, i;
+
+    while(texto[0]!= 'f' && texto[1] != 'i' && texto [2] != 'm'){
+    scanf("%s%*c", texto);  
+    for (i = 0; i <strlen(texto); i++) {
+        if (texto[i] == '0') 
+            printf("zero ");
+
+        if(texto[i] == '1') 
+            printf("um ");
+
+        if(texto[i] == '2') 
+            printf("dois ");
+
+        if(texto[i] == '3') 
+            printf("tres ");
+        
+        if(texto[i] == '4') 
+            printf("quatro ");
+        
+        if(texto[i] == '5') 
+            printf("cinco ");
+        
+    }
+    printf("\n");
+    }
+        return 0;
+    }
