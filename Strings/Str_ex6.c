f958d668ea8fc1c742ec0bd4f78a2ddda9c6bd12
guilherme_miller <gktmiller@gmail.com>
2022-11-10 15:46:54 -0300

    ex6 strings pronto

--- Strings/Str_ex6.c
+++ Strings/Str_ex6.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 #define MAX 100
 
 int main(){
    char frase[MAX];
-   int size,i,valor=0;
+   int i=0,valor=0;
    scanf("%[^\n]%*c",frase);
-   strlen(frase);
-   size = strlen(frase);
-   for (i = 0; i < size; i++)
+   while (frase[i]!='\0')
    {
-      if(frase[i]=='1'){
+      if(frase[i]==' ')
+      {
          valor++;
       }
+      i++;
    }
-   printf("Numero de palvras = %d\n",valor);
+   printf("Numero de palvras = %d\n",valor+1);
    return 0;
 }
