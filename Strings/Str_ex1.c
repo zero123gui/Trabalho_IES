f4a5c7c9d7f3a5213d53fed8b8d2b4d20578e6b8
guilherme_miller <gktmiller@gmail.com>
2022-11-10 12:07:33 -0300

    ex1 strings concluido

--- Strings/Str_ex1.c
+++ Strings/Str_ex1.c
@@ -3,7 +3,20 @@
 #include <locale.h>
 #define MAX 81
 
-int lernum(char *str){//função que determina a quantidade de numeros
+int lerPont(char *str){
+   int cont1,valor=0;
+   char pont;
+   pont = ",.;:!?()-";
+   for(cont1=0; str[cont1]; cont1++){
+      if(str[cont1]==pont){
+         valor++
+      }
+   }
+
+   return valor;
+}
+
+int lerNum(char *str){//função que determina a quantidade de numeros
    int valor=0,cont1;
    for (cont1 = 0; str[cont1]; cont1++)
    {
@@ -19,6 +32,7 @@ int main(){
    printf("Insira uma frase\n");
    fgets(str, MAX, stdin);
    printf("A string tem %zu caracteres", strlen(str));//a função strlen retorna o numero de caracteres da string
-   printf("A string tem %s números\n",lernum(str));
+   printf("A string tem %s pontuação\n",lerNum(str));
+   printf("A string tem %s números\n",lerNum(str));
    return 0;
 }
