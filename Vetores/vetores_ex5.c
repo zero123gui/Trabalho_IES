21fd1cf00115d18682ffcd9758669ed567e45d2c
guilherme_miller <gktmiller@gmail.com>
2022-11-16 21:46:37 -0300

    ex 5 vetores pronto

---
+++ Vetores/vetores_ex5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void scanvet(int vet[]){
+    int i;
+    for (i = 0; i < 10; i++){
+        scanf("%d",&vet[i]);
+    }
+}
+int media (int vet[]){
+    int i, m = 0;
+    for(i = 0; i< 10; i++){
+        m += vet[i];
+    }
+    return m/10;
+}
+int main(){
+int vet[10], i = 0, me = 0;
+
+scanvet(vet);
+me = media(vet);
+for(i = 0; i < 10; i++){
+    if(me == vet[i]){
+        printf("A média é igual ao valor %d\n",vet[i]);
+    }
+}
+    return 0;
+}
